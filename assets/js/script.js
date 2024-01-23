23599628d35a812aec7baf5ee55c120a7a23af30
Saranya <jsaran.bj@gmail.com>
2024-01-23 12:20:16 +0100

    Add favicon to the webpage and style the heading using css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
 console.log("connected")
+
+function startQuiz() {
+    // Get the quiz area element
+    var quizArea = document.querySelector('.game-area');
+
+    // Display the quiz area
+    quizArea.style.display = 'block';
+  }
