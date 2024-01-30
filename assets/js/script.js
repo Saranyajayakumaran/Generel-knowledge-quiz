09d763c1eb588cec5a7c4d7dddf3d046caa9132c
Saranya <jsaran.bj@gmail.com>
2024-01-30 11:53:10 +0100

    Add continue button in the html game info and style it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,42 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let userName="";
-    let currentQuestionIndex=0;
+document.addEventListener('DOMContentLoaded', function () {
+    let userName = "";
+    let currentQuestionIndex = 0;
 
-    let nameInput=document.getElementById("name");
+    /*document.body.style.backgroundImage="url('assets\images\6114100.jpg')";*/
+
+    let nameInput = document.getElementById("name");
     let startBtn = document.querySelector(".start_btn button");
     let exitBtn = document.getElementsByClassName("exit");
     let nextBtn = document.getElementsByClassName("next");
-    
+    let gameArea = document.querySelector(".game-area");
+    let questionElement = document.getElementById("question");
+    let timeLeftElement = document.querySelector(".time .seconds");
+    let optionsContainer = document.getElementById("options");
+
+
+    firstPage();
+
+    /* Event listener for the Start Quiz button*/
+    startBtn.addEventListener("click", startQuiz);
+
 });
 
-
-function startQuiz(){
-    // Hide start button and show game area
-    document.querySelector(".start_btn").style.display = "none";
+function firstPage() {
+    document.querySelector(".heading").style.display = "block";
+    document.querySelector(".start_btn").style.display = "button";
     document.querySelector(".game-info").style.display = "none";
-    document.querySelector(".username").style.display="none";
-    document.querySelector(".game-area").style.display = "block";
+    document.querySelector(".username").style.display = "none";
+    document.querySelector(".game-area").style.display = "none";
+
+}
+
+
+function startQuiz() {
+    // Hide other blocks and show 
+    document.querySelector(".heading").style.display = "block";
+    document.querySelector(".start_btn").style.display = "none";
+    document.querySelector(".game-info").style.display = "block";
+    document.querySelector(".username").style.display = "block";
+    document.querySelector(".game-area").style.display = "none";
 
 }
