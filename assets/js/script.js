934f21894bbbdc1b7a3afa0ab7d871fff8d5cfd7
Saranya <jsaran.bj@gmail.com>
2024-01-30 13:18:35 +0100

    Added some questions to the quizData in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let questionElement = document.getElementById("question");
     let timeLeftElement = document.querySelector(".time .seconds");
     let optionsContainer = document.getElementById("options");
-    let startGame=document.getElementsByClassName("continue");
+    let startGame = document.getElementsByClassName("continue");
 
 
     homePage();
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     /*Event listener for the Continue Button */
 
-    startGame.addEventListener(click,gameSection());
+    startGame.addEventListener(click, gameSection());
 
 });
 
@@ -38,7 +38,7 @@ function homePage() {
 
 
 function startQuiz() {
-    /*show only heading rules and username when click startbutton*/ 
+    /*show only heading rules and username when click startbutton*/
     document.querySelector(".heading").style.display = "block";
     document.querySelector(".start_btn").style.display = "none";
     document.querySelector(".game-info").style.display = "block";
@@ -47,7 +47,7 @@ function startQuiz() {
 
 }
 
-function gameSection(){
+function gameSection() {
     /*show only game area when click continue button*/
     document.querySelector(".heading").style.display = "none";
     document.querySelector(".start_btn").style.display = "none";
@@ -57,3 +57,56 @@ function gameSection(){
 
 
 }
+
+const quizData = [
+    {
+        question: "Which is the largest Spanish-speaking city in the world?",
+        options: ["New york", "Paris", "Mexico", "Frankfurt"],
+        correctAnswer: "Mexico"
+
+    },
+    {
+        question: "How many heart does an Octopus have",
+        options: ["3", "4", "2", "1"],
+        correctAnswer: "3"
+
+    },
+    {
+        questiion: "Where is the strongest human muscle located?",
+        options: ["Tigh", "Foot", "Elbow", "Jaw"],
+        correctAnswer: "Jaw"
+
+    },
+    {
+        question: "Where is Disney's European theme park located?",
+        options: ["New york", "Paris", "Mexico", "Frankfurt"],
+        correctAnswer: "Paris"
+    },
+    {
+        questiion: "Which city is known as 'The Eternal City'",
+        options: ["Venice", "Turin", "Rome", "Pisa"],
+        correctAnswer: "Rome"
+
+    },
+    {
+        questiion: "Which Plnet is closest to the Sun?",
+        options: ["Mercury", "Mars", "Earth", "Neptune"],
+        correctAnswer: "Mercury"
+ 
+    },
+    {
+        questiion: "In wich country people drink more Coffee?",
+        options: ["Ireland", "Germany", "Singapore", "Finland"],
+        correctAnswer: "Finland"
+  
+    }
+    {
+        tiion: "What is the currency of Europe?",
+        options: ["Dollar", "Rupee", "Euro", "Dhiram"],
+        correctAnswer: "Euro"
+  
+    }
+
+]
+
+
