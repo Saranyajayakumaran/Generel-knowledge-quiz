02d24de91a741db61d21c8d717d3bb2a253dae21
Saranya <jsaran.bj@gmail.com>
2024-02-08 12:29:03 +0100

    Chage css properties values and removed blank spaces in code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,90 +1,69 @@
-used: false
 /*Get the questions as arrays and objects inside the array*/
 const quizData = [
     {
         question: "Which is the largest Spanish-speaking city in the world?",
         options: ["New york", "Paris", "Mexico", "Frankfurt"],
         correctAnswer: "Mexico"
-
     },
     {
         question: "How many heart does an Octopus have",
         options: ["3", "4", "2", "1"],
         correctAnswer: "3"
-
-
     },
     {
         question: "Where is the strongest human muscle located?",
         options: ["Tigh", "Foot", "Elbow", "Jaw"],
         correctAnswer: "Jaw"
-
-
     },
     {
         question: "Where is Disney's European theme park located?",
         options: ["New york", "Paris", "Mexico", "Frankfurt"],
         correctAnswer: "Paris"
-
     },
     {
         question: "Which city is known as 'The Eternal City'",
         options: ["Venice", "Turin", "Rome", "Pisa"],
         correctAnswer: "Rome"
-
-
     },
     {
         question: "How many ring have Olympic Flag?",
         options: ["Two", "Five", "six", "Nine"],
         correctAnswer: "Five"
-
     },
     {
         question: "In wich country people drink more Coffee?",
         options: ["Ireland", "Germany", "Singapore", "Finland"],
         correctAnswer: "Finland"
-
-
     },
     {
         question: "What is the currency of Europe?",
         options: ["Dollar", "Rupee", "Euro", "Dhiram"],
         correctAnswer: "Euro"
-
-
     },
     {
         question: "Which planet is known as the “Blue Planet”?",
         options: ["Earth", "Uranus", "Jupiter", "Saturn"],
         correctAnswer: "Earth"
-
     },
     {
         question: "What is the world's largest ocean?",
         options: ["Atlantic Ocean", "Indian Ocean", "Pacific Ocean", "Southern Ocean"],
         correctAnswer: "Pacific Ocean"
-
     },
     {
         question: "Who was the first person to step foot on the moon in 1969?",
         options: ["Neil Armstrong", "Buzz Aldrin", "Michael Collins", "Yuri Gagarin"],
         correctAnswer: "Neil Armstrong"
-
-
     },
     {
         question: "Who painted the Mona Lisa?",
         options: ["Vincent van Gogh", "Leonardo da Vinci", " Pablo Picasso", "Claude Monet"],
         correctAnswer: "Leonardo da Vinci"
-
     },
     {
         question: 'What year did the United States gain independence?',
         options: ["1776", "1676", "1576", "1476"],
         correctAnswer: "1776"
-
-
     },
     {
         question: "Who is known as the “Father of Modern Physics”?",
@@ -102,38 +81,30 @@ const quizData = [
         question: "Which tree is used to make paper from these 4?",
         options: ["Peepal", "Rosewood", "Ashoka", "Bamboo"],
         correctAnswer: "Bamboo"
-
     },
     {
         question: "APJ Abdul Kalam played a crucial role in which space mission?",
         options: ["Chandrayaan-1", "Mangalyan", "GSLV Mk III", "INSAT-3DR"],
         correctAnswer: "Mangalyan"
-
     },
     {
         question: "who is the richest man in the world 2023?",
         options: ["Elon Musk", "Bernard Arnault", "Jeff Bezos", "Bill Gates"],
         correctAnswer: "Elon Musk"
-
     },
     {
         question: "What country has the most islands in the world?",
         options: ["Norway", "Indonesia", "Sweden", "Australia"],
         correctAnswer: "Sweden"
-
     },
     {
         question: "What is the most consumed alcoholic drink in the world?",
         options: ["Beer", "Wine", "Whisky", "Brandy"],
         correctAnswer: "Beer"
-
     }
-
-
 ]
 
 document.addEventListener('DOMContentLoaded', function () {
-
     /*document.body.style.backgroundImage="url('assets\images\6114100.jpg')";*/
 
     const nameInput = document.getElementById("name");
@@ -173,9 +144,8 @@ let answer = [];
 let optionsElement;//option buttons
 let randomNumbers;
 const numOfQuestions = 10;//Number of questions to display in the quiz
-
+/*show only heading and start button in home page*/
 function homePage() {
-    /*show only heading and start button in home page*/
     document.querySelector(".heading").style.display = "block";
     document.querySelector(".start_btn").style.display = "block";
     document.querySelector(".game-info").style.display = "none";
@@ -183,12 +153,9 @@ function homePage() {
     document.querySelector(".game-area").style.display = "none";
     document.getElementById("score").style.display = "none";
     document.getElementById("restart").style.display = "none";
-
 }
-
-
+ /*show only heading rules and username when click startbutton*/
 function startQuiz() {
-    /*show only heading rules and username when click startbutton*/
     document.querySelector(".heading").style.display = "block";
     document.querySelector(".start_btn").style.display = "none";
     document.querySelector(".game-info").style.display = "block";
@@ -196,15 +163,10 @@ function startQuiz() {
     document.querySelector(".game-area").style.display = "none";
     document.getElementById("score").style.display = "none";
     document.getElementById("restart").style.display = "none";
-
 }
-
+ /*show only game area when click continue button*/
 function gameSection() {
-
     /*Display only the game area */
-
-    /*show only game area when click continue button*/
-
     document.querySelector(".heading").style.display = "none";
     document.querySelector(".start_btn").style.display = "none";
     document.querySelector(".game-info").style.display = "none";
@@ -213,10 +175,8 @@ function gameSection() {
     document.getElementById("score").style.display = "none";
     document.getElementById("restart").style.display = "none"
 }
-
+//Dsplay only score area
 function scoreSection() {
-    //Dsplay only score area
-
     document.querySelector(".heading").style.display = "none";
     document.querySelector(".start_btn").style.display = "none";
     document.querySelector(".game-info").style.display = "none";
@@ -225,14 +185,10 @@ function scoreSection() {
     document.getElementById("score").style.display = "block";
     document.getElementById("restart").style.display = "block"
 }
-
-
-
 /*Get user name from the user before starting the quiz*/
 function continueWithUsername() {
     console.log('continueWithUsername');  //to check weather the code enters the function
     const nameInput = document.getElementById("name").value.trim();
-
     const isAlphabets = /^[a-zA-Z\s.]*$/;//The name can have letters and spaces and .period
     /*Get user input*/
     if (nameInput === "") {
@@ -240,22 +196,14 @@ function continueWithUsername() {
     } else if (!isAlphabets.test(nameInput)) {
         alert("Your name can contain only alphabets,space and (.), Please enter a valid name");
     } else {
-
         /*Display the game section when user enters the name*/
         gameSection();
-
         randomNumbers = getRandomUniqueNumbers(numOfQuestions, 0, (quizData.length - 1));
-
         console.log("Generated unique random numbers:", randomNumbers);//To check the generaed unique numbers in the console
-
         nextQuestion();
-
         alert("Quiz started! Welcome to GK QUIZ, " + nameInput + "!");
-
     }
 }
-
-
 /**
  * Generating random numbers as array
  * @param {number} amount 
@@ -265,61 +213,43 @@ function continueWithUsername() {
  */
 function getRandomUniqueNumbers(amount, firstNum, maxNum) {
     if (amount > (maxNum - firstNum + 1)) {
-
         return [];
     }
-
     let randomUniqueNumber = [];
-
     while (randomUniqueNumber.length < amount) {
         let randomNumber = Math.floor(Math.random() * (maxNum - firstNum + 1)) + firstNum;
-
         // Check weather the number already in random number array
         if (!randomUniqueNumber.includes(randomNumber)) {
-
             randomUniqueNumber.push(randomNumber);
             //if the number is already there it will go get the other random number
         }
-
     }
-
     return randomUniqueNumber;
 }
-
+/*Get random index*/
 function getIndex() {
-
     if (questionNumber < randomNumbers.length) {
-
         console.log('random number', randomNumbers[questionNumber]);  // to check the random question  in console
-
         return randomNumbers[questionNumber];
-    }
-    else {
+    }else {
         console.error("Error: question number exceeded maximum number of range.");// to check in the console
         return [];
     }
 }
-
 let currentQuestionIndex = 0;//declaring globally to use in other functions
-
+/*Display question in the quiz */ 
 function showQuestion() {
-
     const questionElement = document.getElementById('question');
     const optionsElement = document.getElementById('options');
-
     /*get random index */
     currentQuestionIndex = getIndex();
-
+    /*assign the current index to current question*/
     const currentQuestion = quizData[currentQuestionIndex];
-
-
     questionElement.innerHTML = (questionNumber + 1) + "." + currentQuestion.question;//Display the questions with question number
     optionsElement.innerHTML = ''; //clear  the text already in option buttons
-
     // display the answer options of selected questions
     for (let index = 0; index < currentQuestion.options.length; index++) {  //Loopting through index value 
         const option = currentQuestion.options[index];
-
         const optionButton = document.createElement('button');
         optionButton.textContent = option;
         optionButton.classList.add('btn');
@@ -327,53 +257,36 @@ function showQuestion() {
         optionsElement.appendChild(optionButton);
     }
 }
-
+/*Go to next question each ime the function calls*/ 
 function nextQuestion() {
-
     if (questionNumber < numOfQuestions) {
-
         showQuestion();
         startTimer();
-
         questionNumber++;//increment the question number each time clicks the next button
     }
-
     else {
-
-
         alert("quiz finished");
         displayScore();
-
     }
 }
-
-
 /*Code to select option button from the user and check it is correct or not*/
-
 function selectAnswer(selectedOption) {
-
     const currentQuestion = quizData[currentQuestionIndex];
-
     const optionButtons = document.querySelectorAll(".btn");
     //display the correct answer buton and wrong anser in red
-
     optionButtonsDisable();// call the function to disable the option buttons 
-
     for (let i = 0; i < optionButtons.length; i++) {
         const button = optionButtons[i];
-
         if (button.textContent === currentQuestion.correctAnswer) {
             button.classList.add("correct");
         } else if (button.textContent === selectedOption) {
             button.classList.add("incorrect");
         }
     }
-
     if (selectedOption === currentQuestion.correctAnswer) {
         score++;
         console.log("score increased:" + score);
     }
-
 }
 //option buttons disabled to avoid further clicks after select the answer 
 function optionButtonsDisable() {
@@ -382,49 +295,35 @@ function optionButtonsDisable() {
         button.disabled = true;
     });
 }
-
+/*Display score of the user*/
 function displayScore() {
-
     scoreSection();
     console.log("score increased:" + score);// to check the score in console
-    /*let scoreValue = document.getElementById("score");*/
     const nameInput = document.getElementById("name").value.trim();
     const generalMessage=document.getElementById("message");
     let userScore=document.getElementById("score-value");
     let feedback=document.getElementById("feedback");
-    /*scoreValue.innerHTML = '';*/
-    generalMessage.innerHTML='';
-    userScore.innerHTML='';
-    feedback.innerHTML='';
-    generalMessage.textContent=nameInput;
+    generalMessage.textContent=` ${nameInput}`;
     userScore.textContent=score + " out of " + numOfQuestions ;
-
     if (score < 5) {
-
         feedback.textContent="You can improve Next time";
     } else {
        
         feedback.textContent="Great job! Keep It Up";
     }
-
 }
-
-
 function exitQuiz() {
     // get confirmation from the user
     const confirmExit = confirm("Are you sure you want to exit the quiz?");
-
     // If the user confirms, end the quiz
     if (confirmExit) {
-
         alert("Quiz exited. Thank you!");
         resetQuiz();
-
     }
 }
-
+/*reset all the values and ui elements*/
 function resetQuiz() {
-    // Reset everyting before go to home page
+// Reset everyting before go to home page
     userName = "";
     score = 0;
     selectedQuestions.length = 0;
@@ -435,13 +334,11 @@ function resetQuiz() {
     answer.length = 0;
     questionNumber = 0;
     randomNumbers = undefined;
-
     // Reset UI elemens
     let nameInput = document.getElementById("name");
     let scoreValue = document.getElementById("score");
     let questionElement = document.getElementById("question");
     let optionsContainer = document.getElementById("options");
-
     nameInput.value = "";
     scoreValue.innerHTML = "";
     questionElement.innerHTML = "";
@@ -450,33 +347,27 @@ function resetQuiz() {
 }
 
 let globalCountDownTimer_Id = 0;
-
+/*start the timer with 15 seconds and reduce the time for 1 second*/ 
 function startTimer() {
     let remainingTime = seconds;
     let timeLeftElement = document.querySelector(".time-left");
-
     /* Check already a timer instance created and running then stop before new instance*/
     if (globalCountDownTimer_Id != 0) {
         stopTimer();
     }
-
     globalCountDownTimer_Id = setInterval(() => {    // which returns an id and store in the variable
         timeLeftElement.innerHTML = `Time left: ${remainingTime}`;
-
         if (remainingTime <= 0) {
             stopTimer();
-
             // Check if there are more questions or show the final result
             if (questionNumber < numOfQuestions) {
                 setTimeout(() => {
-
                     nextQuestion();
                 }, 2000); // Wait for 2 seconds before loading the next question
             } else {
                 console.log('Quiz completed.');  // You can replace this with your desired action for quiz completion
             }
         }
-
         remainingTime--;
     }, 1000); // Update the time every 1 second
 }
